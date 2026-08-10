dcf484a9c33324b4b1d92509d8b0b13c407edb54
mincy1224 <artemis03668@gmail.com>
2026-08-10 13:59:48 +0800

    chore: export AgentServer from top-level mpmt

--- src/mpmt/__init__.py
+++ src/mpmt/__init__.py
@@ -2,18 +2,9 @@
 @author  mincy
 """
 
-# ——— channel ———
 from mpmt import channels                       # noqa: F401
-
-# ——— C++ bindings ———
 from mpmt._mpmt import *                        # noqa: F401,F403
-
-# ——— utility ———
 from mpmt.util import bf_param                  # noqa: F401,E402
-
-
-# ——— domain services ———
 from mpmt.querier import Querier               # noqa: F401,E402
-
-# ——— MPMT roles ———
 from mpmt.set_holder import SetHolder          # noqa: F401,E402
+from mpmt.agent_server import AgentServer      # noqa: F401,E402
